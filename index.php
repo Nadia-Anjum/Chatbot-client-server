dbef730b9258cae6cd3d1faaeb8eb7ba6a9ef2d2
Nadia-Anjum <diaanjum@outlook.dk>
2023-09-04 12:34:20 +0200

    Tilføjet character count i php filen og stylet den

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chatbot</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="chat-container">
         <div class="chatbot">
@@ -14,48 +15,61 @@
                 <h2>Chatbot</h2>
             </header>
 
-        <ul class="chatbox">
-            <li class="message received">Hello, how can i help you?</li>
+            <ul class="chatbox">
+                <li class="message received">Hello, how can i help you?</li>
 
-            <li class="message sent"> 
-                <?php $myInput = $_GET['myInput']; {
-                echo $_GET['myInput']; } ?>
-             </li>
+                <li class="message sent">
+                    <?php $myInput = $_GET['myInput']; {
+                        echo $_GET['myInput'];
+                    } ?>
+                </li>
 
-            <li class="message received">
-                <?php if($myInput == "What can you do?"){
-                echo "I can answer your questions";
-
-                } elseif ($myInput == "How old are you?") {
-                echo "I don't have an age";
-                }
-                
-                elseif ($myInput == "Hvor gammel er du?") {
-                    echo "Jeg har ikke en alder";
-                    }
-
-                elseif ($myInput == "Hello") {
+                <li class="message received">
+                    <?php if ($myInput == "What can you do?") {
+                        echo "I can answer your questions";
+                    } elseif ($myInput == "How old are you?") {
+                        echo "I don't have an age";
+                    } elseif ($myInput == "Hvor gammel er du?") {
+                        echo "Jeg har ikke en alder";
+                    } elseif ($myInput == "Hello") {
                         echo "Hello Human";
-                    } 
+                    } elseif ($myInput == "Who am i?") {
+                        echo "I don't know who you are";
+                    } else echo "I don't understand";
 
-                elseif ($myInput == "Who am i?") {
-                echo "I don't know who you are";
+                    ?>
+                </li>
+                <p id="tegnTæller">Antal tegn: 0</p>
+            </ul>
 
-                } else echo "I don't understand";
 
-                ?>
-        </li>
-
-        </ul>
-        
-        
-        <form class="user-input" method="get" action="?">
-            <input type="text" name="myInput" placeholder="Type your message...">
-            <button type="submit" value="CLICK ME">Send</button>
-        </form>
+            <form class="user-input" method="get" action="?">
+                <input type="text" name="myInput" for="tekstfelt" id="tekstfelt" placeholder="Type your message...">
+                <button type="submit" value="CLICK ME">Send</button>
+            </form>
+        </div>
     </div>
-</div>
+    <script>
+        // Hent inputfeltet og tegntæller-paragraph ved hjælp af querySelector
+        const tekstfelt = document.querySelector("#tekstfelt");
+        const tegnTæller = document.querySelector("#tegnTæller");
+
+        // Tilføj en event listener til inputfeltet, der lytter efter tastetryk
+        tekstfelt.addEventListener("input", opdaterTegnTæller);
+
+        // Funktion til at opdatere tegntælleren
+        function opdaterTegnTæller() {
+            // Hent længden af tekst i inputfeltet
+            const tekstLængde = tekstfelt.value.length;
+
+            // Opdater tekst i tegntæller-paragraphen
+            tegnTæller.textContent = `Antal tegn: ${tekstLængde}`;
+        }
+    </script>
+
+
 
 
 </body>
+
 </html>

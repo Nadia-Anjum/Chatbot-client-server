b8b1ee8795adbb19020c66bad5d3d7bf1ee456e2
Nadia-Anjum <diaanjum@outlook.dk>
2023-10-23 10:59:59 +0200

    tilføjet kommentar

--- api-backend/api/history2.php
+++ api-backend/api/history2.php
@@ -1,15 +1,17 @@
 <?php 
-
+// Check if the 'history' session variable exists
 if (!isset($_SESSION['history'])) {
     $_SESSION['history'] = array(); // Initialize history as an empty array if it doesn't exist
 }
 
+// Add user input and chatbot response to the chat history
 function addToChatHistory($userInput, $chatbotResponse)
 {
     $_SESSION['history'][] = $userInput;
     $_SESSION['history'][] = $chatbotResponse;
 }
 
+// Retrieve the entire chat history from the session
 function getChatHistory()
 {
     return $_SESSION['history'];
